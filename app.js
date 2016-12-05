360fbdbe8120d4948f01ed3ee2293c91ee3fd741
Patricio Vargas <pevargas@valdosta.edu>
2016-12-05 00:47:33 -0500

    installe body parser

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ var express = require('express')
 var app = express()
 var morgan = require('morgan');
 var mongoose = require('mongoose');
-//var mongojs = require('mongojs');
+var bodyParser = require('body-parser');
+
+//My one modules
 var User = require('./app/models/user');
-var Company = require('./app/models/user');
+var Company = require('./app/models/company');
+
 //Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
+
+//Connection to DB
 db = mongoose.connect('mongodb://wearetamo:Policia9@ds119768.mlab.com:19768/jobersity',['jbusers'],function(err){
     if(err){
         console.log("Not connection to DB" + err);
@@ -17,9 +24,24 @@ db = mongoose.connect('mongodb://wearetamo:Policia9@ds119768.mlab.com:19768/jobe
     }
 });
 //Routes
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.send('Hello World!')
-})
+})*/
+
+//Creating Users
+app.post('/users',function(req,res){
+    res.send('testing users route');
+    var user = new User();
+    user.username = req.body.username;
+    user.password = req.body.password;
+    //user.firstName = req.body.firstName;
+    //user.lastName = req.body.lastName;
+    user.email = req.body.email;
+    //user.major = req.body.major;
+    //Save user
+    user.save(); 
+    res.send('user created!');   
+});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
--- app/models/user.js
+++ app/models/user.js
@@ -8,12 +8,12 @@ var UserSchema = new Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
     email:{type: String, lowercase: true, required: true, unique: true},
-    /*major: {type: String, required: true},
+    major: {type: String, required: true},
     jobsApplied:[{
         title: String,
         description: String,
         position: String
-    }]*/
+    }]
 });
 
 module.exports = mongoose.model('User', UserSchema);
