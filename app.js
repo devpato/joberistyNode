c74f561271bcfbe69bb8754c62c0de1b6c9a95f7
Patricio Vargas <pevargas@valdosta.edu>
2016-12-06 01:54:12 -0500

    route for home page

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var router = express.Router();
 var appRoutes = require('./app/routes/api')(router)
+var path = require('path');
 
 //Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 app.use('/api/',appRoutes);
 
-//Connection to DB
+//Connection to db in the cloud
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://wearetamo:Policia9@ds119768.mlab.com:19768/jobersity',function(err){
     if(err){
@@ -22,6 +24,11 @@ mongoose.connect('mongodb://wearetamo:Policia9@ds119768.mlab.com:19768/jobersity
     else{
         console.log("Connected to DB");
     }
+});
+
+//Showing home page
+app.get('*',function(req,res){
+    res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
 });
 
 app.listen(process.env.PORT || 5000, function () {
