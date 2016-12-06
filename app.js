4eb25dd1e416cf2c7485bc074a7b5b5ebcc45653
Patricio Vargas <pevargas@valdosta.edu>
2016-12-06 01:45:41 -0500

    router file created (api)

--- app.js
+++ app.js
@@ -3,18 +3,16 @@ var app = express()
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var router = express.Router();
+var appRoutes = require('./app/routes/api')(router)
 
-//My one modules
-var User = require('./app/models/user');
-var Company = require('./app/models/company');
 //Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
-
+app.use('/api/',appRoutes);
 
 //Connection to DB
-
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://wearetamo:Policia9@ds119768.mlab.com:19768/jobersity',function(err){
     if(err){
@@ -24,64 +22,6 @@ mongoose.connect('mongodb://wearetamo:Policia9@ds119768.mlab.com:19768/jobersity
     else{
         console.log("Connected to DB");
     }
-});
-
-//Creating Users
-app.post('/users',function(req,res){
-    var user = new User();
-    user.username = req.body.username;
-    user.password = req.body.password;
-    user.email = req.body.email;
-    user.firstName = req.body.firstName;
-    user.lastName = req.body.lastName;
-    user.major = req.body.major;
-    if(req.body.username == null || req.body.username == '' ||
-       req.body.password == null || req.body.password == '' ||
-       req.body.email == null || req.body.email == '' ||
-       req.body.firstName == null || req.body.lastName == '' ||
-       req.body.major == null || req.body.major == ''
-      ){
-        res.send('Ensure all the required files have been provided');
-    }
-    else{
-        user.save(function(err){
-        if(err){
-            res.send("User already exists");
-        }
-        else{
-            res.send('User created!')
-        }
-    }); 
-    }
-      
-});
-//Creating Company
-app.post('/company',function(req,res){
-    var company = new Company();
-    company.username = req.body.username;
-    company.password = req.body.password;
-    company.email = req.body.email;
-    company.name = req.body.name;
-    company.address = req.body.address;
-    company.website = req.body.website;
-    company.telephone = req.body.telephone;
-    if(req.body.username == null || req.body.username == '' ||
-       req.body.password == null || req.body.password == '' ||
-       req.body.email == null || req.body.email == '' ||
-       req.body.name == null || req.body.name == ''
-      ){
-        res.send('Ensure all the required files have been provided');
-    }
-    else{
-        company.save(function(err){
-            if(err){
-                res.send("Company already exists");
-            }
-            else{
-                res.send('Company created!')
-            }
-        });
-    } 
 });
 
 app.listen(process.env.PORT || 5000, function () {
---
+++ app/routes/api.js
@@ -0,0 +1,65 @@
+//My one modules
+var User = require('../models/user');
+var Company = require('../models/company');
+
+module.exports = function(router){
+    //Creating Users
+    router.post('/users',function(req,res){
+        var user = new User();
+        user.username = req.body.username;
+        user.password = req.body.password;
+        user.email = req.body.email;
+        user.firstName = req.body.firstName;
+        user.lastName = req.body.lastName;
+        user.major = req.body.major;
+        if(req.body.username == null || req.body.username == '' ||
+        req.body.password == null || req.body.password == '' ||
+        req.body.email == null || req.body.email == '' ||
+        req.body.firstName == null || req.body.lastName == '' ||
+        req.body.major == null || req.body.major == ''
+        ){
+            res.send('Ensure all the required files have been provided');
+        }
+        else{
+            user.save(function(err){
+                if(err){
+                    res.send("User already exists");
+                }
+                else{
+                    res.send('User created!')
+                }
+            }); 
+        }
+        
+    });
+    
+    //Creating Company
+    router.post('/company',function(req,res){
+        var company = new Company();
+        company.username = req.body.username;
+        company.password = req.body.password;
+        company.email = req.body.email;
+        company.name = req.body.name;
+        company.address = req.body.address;
+        company.website = req.body.website;
+        company.telephone = req.body.telephone;
+        if(req.body.username == null || req.body.username == '' ||
+        req.body.password == null || req.body.password == '' ||
+        req.body.email == null || req.body.email == '' ||
+        req.body.name == null || req.body.name == ''
+        ){
+            res.send('Ensure all the required files have been provided');
+        }
+        else{
+            company.save(function(err){
+                if(err){
+                    res.send("Company already exists");
+                }
+                else{
+                    res.send('Company created!')
+                }
+            });
+        } 
+    });
+    return router;
+}
