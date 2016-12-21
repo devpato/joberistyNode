66832a8f14158298bf058a58f4f698a23f86d9aa
Patricio Vargas <pevargas@valdosta.edu>
2016-12-20 22:21:07 -0500

    inserting user into the DB

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -1,10 +1,11 @@
 
 angular.module('userController',[])
 
-.controller('regCtrl',function(){
+.controller('regCtrl',function($http){
     console.log('testing reg controller');
     this.regUser = function(regData){
         console.log("form submitted");
         console.log(this.regData);
+        $http.post("/api/users",this.regData);
     };
 });
